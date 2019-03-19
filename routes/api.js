c4c360bf7bec5b5bea9419cb628248f00e9572bf
Kasper Lind Sørensen <kasper.lind.sorensen@gmail.com>
2019-03-19 22:42:00 +0100

    Updated 'api/pollen' to require pollen type and location and pass these to Yesterday's Pollen.

--- routes/api.js
+++ routes/api.js
@@ -3,12 +3,20 @@ const request = require('request-promise-native')
 const router = express.Router()
 
 router.get('/pollen', async function (req, res, next) {
+  if (!req.query.pollenType || !req.query.location) {
+    return res.sendStatus(400)
+  }
+
   var date = new Date()
   date.setDate(date.getDate() + 1)
   var options = {
     method: 'GET',
     uri: `${process.env.YESTERDAYS_POLLEN_URL}/api/pollen/${date.toISOString()}`,
-    json: true
+    json: true,
+    qs: {
+      pollentype: req.query.pollenType,
+      location: req.query.location
+    }
   }
   var result
   try {
@@ -18,7 +26,11 @@ router.get('/pollen', async function (req, res, next) {
     res.status(500).send('Unable to retrieve pollen numbers.')
   }
   if (result) {
-    res.json(result.PredictedPollenCount)
+    res.json({
+      predictedPollenCount: result.PredictedPollenCount,
+      city: result.Location.City,
+      country: result.Location.Country
+    })
   }
 })
 
